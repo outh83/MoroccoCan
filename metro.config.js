903e2f9006024586631a4eaba238af06ea3986a9
outh83 <h.outhman@gmail.com>
2025-07-09 14:56:14 +0200

    Fix TypeScript module resolution with proper configuration

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,8 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Enable TypeScript support
+config.resolver.sourceExts.push('ts', 'tsx');
+config.transformer.babelTransformerPath = require.resolve('metro-react-native-babel-transformer');
+
 module.exports = config;
